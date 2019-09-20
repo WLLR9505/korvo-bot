86fd9f1d92b5c40b25a83a8148080443415dccdf
Weuller Jr. Souza <weuller9505@live.com>
2019-09-20 12:08:42 -0300

    v2

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # korvo-bot
-Discord bot
---
+++ deleteAll.js
@@ -0,0 +1,12 @@
+module.exports = async (client, message) => {
+    let i = 0
+    let fetched;
+    fetched = await message.channel.fetchMessages({ limit: 100 });
+    while (fetched.size >= 2) {
+        await message.channel.bulkDelete(fetched.size)
+        i += fetched.size
+        fetched = await message.channel.fetchMessages({ limit: 100 });
+    }
+    message.channel.send(`Foram deletadas **${i} mensagens**`)
+    console.log(`Foram deletadas ${i} mensagens do canal: ${message.channel.name}`);
+}

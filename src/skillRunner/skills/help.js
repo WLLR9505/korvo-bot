738d1572a545d726690c3b6fa6845a4e0feb0db2
Weuller Jr. Souza <weuller9505@live.com>
2020-02-06 20:12:50 -0300

    update Help.js

--- src/skillRunner/skills/help.js
+++ src/skillRunner/skills/help.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 var helps = JSON.parse(fs.readFileSync('./src/skillRunner/skills/help.json'));
+var version = JSON.parse(fs.readFileSync('./package.json')).version;
 
 module.exports = (msg) => {
     var helpMessage = '';
@@ -9,7 +10,7 @@ module.exports = (msg) => {
         helpMessage += `**${helps[i].command}**\n`;
         helpMessage += `${helps[i].description}\n\n`;
     }
-    msg.channel.send(helpMessage).then((msg) => {
+    msg.channel.send(helpMessage + `\n\n**versão: ${version}**`).then((msg) => {
         msg.delete(10000);
     });
     msg.delete(100);
